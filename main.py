17de5374507d99ef168ddd7ace2068071d1b7032
skyler-vestal <dinoswarleaf@gmail.com>
2020-05-08 16:34:40 -0500

    Adds steam buyer to seller

---
+++ main.py
@@ -0,0 +1,12 @@
+def getSellerPrice(buyer):
+    buyer_rd = round(buyer/11.5, 3)
+    buyer_rd = buyer_rd - buyer_rd % .01
+    deduct = max(.01, buyer_rd)
+    buyer_rd = round(buyer/23, 3)
+    buyer_rd = buyer_rd - buyer_rd % .01
+    deduct += max(.01, buyer_rd)
+    seller = round(buyer - deduct, 2)
+    return seller
+
+for i in range(1, 100):
+    print(i/100, getSellerPrice(i/100))
