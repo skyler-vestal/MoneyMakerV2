b9949123a452accff472a948abdfe3e3264667e0
skyler-vestal <dinoswarleaf@gmail.com>
2020-05-09 18:49:08 -0500

    Adds steam bot code

--- data_gather/getData.js
+++ data_gather/getData.js
@@ -1,14 +1,82 @@
-var Steam = require('steam'),
+var fs = require("fs");
+info = fs.readFileSync("requested_data/user.info");
+data = info.toString().split("\r\n");
+username = data[0];
+password = data[1];
+shared_secret = data[2]; 
+
+
+var Steam = require("steam"),
+    csgo = require("csgo"),
+    util = require("util"),
+    Totp = require('steam-totp'),
     steamClient = new Steam.SteamClient(),
     steamUser = new Steam.SteamUser(steamClient),
+    steamFriends = new Steam.SteamFriends(steamClient),
     steamGC = new Steam.SteamGameCoordinator(steamClient, 730),
-    csgo = require('csgo'),
-    CSGO = new csgo.CSGOClient(steamUser, steamGC, false);
-   
+    CSGOCli = new csgo.CSGOClient(steamUser, steamGC, false);
+    readlineSync = require("readline-sync")
+    readline = require("readline")
+
+    var onSteamLogOn = function onSteamLogOn(response){
+        if (response.eresult == Steam.EResult.OK) {
+            util.log('Logged in!');
+        }
+        else
+        {
+            util.log('error, ', response);
+            process.exit();
+        }
+        steamFriends.setPersonaState(Steam.EPersonaState.Busy);
+        util.log("Logged on.");
+
+        util.log("Current SteamID64: " + steamClient.steamID);
+        util.log("Account ID: " + CSGOCli.ToAccountID(steamClient.steamID));
+
+        CSGOCli.launch();
+
+        CSGOCli.on("unhandled", function(message) {
+            util.log("Unhandled msg");
+            util.log(message);
+        });
+
+        CSGOCli.on("ready", function() {
+            util.log("node-csgo ready.");
+                
+                console.log("item request");
+                //steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M2590076190517757054A17842144863D7100494772439222079
+                CSGOCli.itemDataRequest("0", "17842144863", "7100494772439222079", "2590076190517757054");
+                
+                CSGOCli.on("itemData", function(itemdata) {
+                    console.log(itemdata);
+                });
+            });
+
+        CSGOCli.on("unready", function onUnready(){
+            util.log("node-csgo unready.");
+        });
+
+        CSGOCli.on("unhandled", function(kMsg) {
+            util.log("UNHANDLED MESSAGE " + kMsg);
+        });
+    },
+    onSteamSentry = function onSteamSentry(sentry) {
+        util.log("Received sentry.");
+        require('fs').writeFileSync('sentry', sentry);
+    },
+    onSteamServers = function onSteamServers(servers) {
+        util.log("Received servers.");
+        fs.writeFile('servers.json', JSON.stringify(servers, null, 2));
+    }
+
+var logOnDetails = {
+    "account_name": username,
+    "password": password,
+    "two_factor_code": Totp.generateAuthCode(shared_secret)
+};
+
 steamClient.connect();
-steamClient.on('connected', function() {
-    steamUser.logOn({
-    account_name: 'username',
-    password: 'password'
+steamClient.on('connected', function(){
+        steamUser.logOn(logOnDetails);
     });
-});
+steamClient.on("logOnResponse", onSteamLogOn);
